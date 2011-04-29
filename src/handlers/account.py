3b5eefac4550ecd9ad234422de92c01c90f488ed
Boris Bluntschli <boris@bluntschli.ch>
2011-04-29 02:55:21 +0200

    Added realm list

---
+++ src/handlers/account.py
@@ -0,0 +1,41 @@
+from google.appengine.ext.webapp import util, template
+
+from hashlib import sha1
+import random
+
+from main import BasicRequestHandler
+from models import User
+
+class AccountHandler(BasicRequestHandler):
+    def get(self):
+        if self.session.get('user', None) is not None:
+            self.redirect('/')
+            return
+            
+        self.render('templates/login.html', {})
+        
+    def post(self):
+        if self.session.get('user', None) is not None:
+            self.redirect('/')
+            return
+            
+        # Verify login information
+        user = User.all().filter('name =', self.request.get('username')).get()
+        form_errors = {}
+        
+        if user is None:
+            form_errors['username'] = 'A user with this name doesn\'t exist.'
+        elif not user.check_password(self.request.get('password')):
+            form_errors['password'] = 'Password is incorrect.'
+        
+        # Render login page (same as GET request)
+        if len(form_errors) > 0:
+            self.render('templates/login.html', {
+                'form': dict([(key, self.request.get(key)) for key in self.request.arguments()]),
+                'form_errors': form_errors
+            })
+            return
+        
+        # We're logged in now
+        self.session['user'] = user.name
+        self.redirect('/')
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import os
 import sys
@@ -9,10 +10,25 @@ from google.appengine.ext.webapp import util, template
 
 import appengine_utilities.sessions
 
+def load_config():
+    result = {}
+    for conf in glob.glob('config/*.json'):
+        try:
+            if conf.endswith('.sample.json'):
+                continue
+            logging.info(conf)
+            subconfig = json.load(open(conf, 'r'))
+            name = os.path.splitext(os.path.basename(conf))[0]
+            result[name] = subconfig
+        except Exception, e:
+            logging.error('Could not load subconfig "%s": %s' % (conf, e))
+    return result
+
 class BasicRequestHandler(webapp.RequestHandler):
     def initialize(self, request, response):
         webapp.RequestHandler.initialize(self, request, response)
         self.session = appengine_utilities.sessions.Session(writer='datastore')
+        self.config = load_config()
     
     def get_base_url(self):
         return '../'*self.request.path.count('/')    
@@ -20,7 +36,8 @@ class BasicRequestHandler(webapp.RequestHandler):
     def render(self, template_path, values):
         base_values = {
             'base_url': self.get_base_url(),
-            'session': self.session
+            'session': self.session,
+            'config': self.config
         }
         base_values.update(values)
         self.response.out.write(template.render(template_path, base_values))
@@ -33,6 +50,7 @@ from handlers.login import LoginHandler
 from handlers.logout import LogoutHandler        
 from handlers.user_registration import UserRegistrationHandler, \
                                        SuccessfulRegistrationHandler       
+from handlers.account import AccountHandler                                       
 
 def main():   
     debug = os.environ['SERVER_SOFTWARE'].startswith('Development/')
@@ -41,7 +59,8 @@ def main():
         ('/register/?', UserRegistrationHandler),
         ('/register/thanks/?', SuccessfulRegistrationHandler),
         ('/login/?', LoginHandler),
-        ('/logout/?', LogoutHandler)
+        ('/logout/?', LogoutHandler),
+        ('/account/?', AccountHandler)
     ]
     application = webapp.WSGIApplication(handlers, debug=debug)
     util.run_wsgi_app(application)    
