fa94eb7d9c901707974d231d1e6102fee3b394fa
Boris Bluntschli <boris@bluntschli.ch>
2011-04-29 11:15:41 +0200

    User name validation

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from hashlib import sha1
+import re
 
 from google.appengine.ext import webapp, db
 from django.core.validators import email_re
@@ -31,8 +32,10 @@ class User(db.Model):
     def can_register(username, email):
         result = {}
         
-        # Make sure username is not in use
-        if User.all().filter('name =', username).count(limit=1) > 0:
+        # Make sure username is valid and not in use
+        if not re.match('[0-9_-a-zA-Z]', username):
+            result['username'] = 'Username must only contain alphanumeric characters and the special characters -, _.'
+        elif User.all().filter('name =', username).count(limit=1) > 0:
             result['username'] = 'The username %s is already in use.' % username
             
         # Email has to be valid and unique
